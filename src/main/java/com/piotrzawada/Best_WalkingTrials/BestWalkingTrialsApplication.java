61ce10fe68350be7d11a67380ff7727a6aea6f6f
Piotr <piotrzawada08@gmail.com>
2024-04-26 20:03:56 +0100

    feat: initialize new spring app #1
    tech
    - Java 17
    - Gradle Groove
    
    dependencies:
    - Spring Web
    - Lombok

---
+++ src/main/java/com/piotrzawada/Best_WalkingTrials/BestWalkingTrialsApplication.java
@@ -0,0 +1,13 @@
+package com.piotrzawada.Best_WalkingTrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestWalkingTrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestWalkingTrialsApplication.class, args);
+	}
+
+}
